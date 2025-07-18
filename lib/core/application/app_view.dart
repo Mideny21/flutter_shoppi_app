dbdebf2de3aa2403458a6c29f9afe55be3a1dcfd
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2025-07-18 10:48:27 +0300

    added translation support

--- lib/core/application/app_view.dart
+++ lib/core/application/app_view.dart
@@ -4,6 +4,8 @@ import 'package:shoppi/core/di/injection.dart';
 import 'package:shoppi/core/router/app_route_observer.dart';
 import 'package:shoppi/core/router/app_router.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,6 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: appRouter.config(navigatorObservers: () => [MyObserver()]),
       title: FlavorConfig.instance.values.appName,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale('sw'),
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,12 +1,22 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 @RoutePage()
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        // The [AppBar] title text should update its message
+        // according to the system locale of the target platform.
+        // Switching between English and Spanish locales should
+        // cause this text to update.
+        title: Text(AppLocalizations.of(context)!.helloWorld),
+      ),
+    );
   }
 }
